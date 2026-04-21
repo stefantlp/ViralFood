22a1d9e91a548bc00bd579e163176be0a3f6acf6
ErrorsEverywhere <amvt2202@gmail.com>
2026-04-22 00:13:52 +0300

    Initializare Set-up

---
+++ services/menu-service/src/test/java/com/viralfood/menuservice/MenuServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viralfood.menuservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenuServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
